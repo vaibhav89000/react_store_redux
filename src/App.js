578872f2152d31566b0c473b7020686ad00a42ac
vaibhav89000 <vaibhav89000@gmail.com>
2022-12-24 22:39:54 +0530

    redux store is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { incNumber,decNumber } from './actions';
+import { useDispatch,useSelector } from 'react-redux';
 
 function App() {
+  const myState = useSelector((state)=>state.number);
+  console.log(myState)
+  const dispatch = useDispatch();
+
+  const handleIncrement = () =>{
+    dispatch(incNumber(10))
+  }
+
+  const handleDecrement = () =>{
+    dispatch(decNumber(10))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello world {myState}
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
     </div>
   );
 }
